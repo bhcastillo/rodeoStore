617f3d56bf9f018c0487f2f3b9716ef0dc278e64
Brayan Castillo <bhcastillor@gmail.com>
2020-10-11 20:58:57 -0500

    filter products by quantity

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,7 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 //Services
 import { ProductsService } from 'src/app/services/products.service';
-import { IProduct } from 'src/app/interfaces/product';
 
 @Component({
   selector: 'app-search',
@@ -11,7 +10,8 @@ import { IProduct } from 'src/app/interfaces/product';
 export class SearchComponent implements OnInit {
   @Input()productsData;
   @Input()categoriesData;
-  itemsType= [{ id:1,name:'Más Vendido'},{ id:2,name:'Disponibles'},{ id:3,name:'Agotados'}]
+  itemsType = [{ id:1,name:'Más Vendido'},{ id:2,name:'Disponibles'},{ id:3,name:'Agotados'}];
+  itemsQuantity = [{id:1, name:'Más de $30.000'},{id:2, name:'Menos de $10.000'}]
   constructor(public productsService:ProductsService) { }
   
   ngOnInit(): void {
@@ -31,6 +31,9 @@ export class SearchComponent implements OnInit {
   }
   clearSelectedType(){
     this.productsService.clearSelectedType();
+  }
+  clearSelectedQuantity(){
+    this.productsService.clearSelectedQuantity();
   }
 
 }
--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -21,7 +21,9 @@ export class ProductsService {
   //variable for filter category
   selectedCategory:string;
   //variable for filter type product
-  selectType:string;
+  selectedType:string;
+  //variable for filter quantity product
+  selectedQuantity:string;
 
   constructor(private http: HttpClient) {
     this._getAllData();
@@ -84,42 +86,70 @@ export class ProductsService {
     const filterProductsData = productsData.filter((product:IProduct)=>{
         //filter products by category
       if (groupSelectOption ==='categories'){
-        for( const key of Object.keys(product.categories)){
+        return this.filterForCategory(product,idSelectedOption)
+      }
+    //filter products by types
+      else if (groupSelectOption === 'types') {
+        return this.filterForType(product, idSelectedOption);
+      }
+      //filter products by quantity
+      else{
+        return this.filterForQuantity(product, idSelectedOption);
+      }
+    });
+    this.productsData$.next(filterProductsData);
+  }
+  private filterForCategory(product:IProduct, idSelectedOption:number){
+    for( const key of Object.keys(product.categories)){
           if (product.categories[key] === idSelectedOption){
             return true;
           }
         }
         return false;
-      }
-    //filter products by types
-    else {
-      switch (idSelectedOption) {
-        //filter products by bestseller
-        case 1:
-          if (product.bestSeller) return true;
-          break;
-        //filter porducts by available
-        case 2:
-          if (product.quantity>0)return true;
-          break;
-        //filter products by Exhausted 
-        case 3:
-          if (product.quantity<1)return true;
-        default:
-          return false;
-          break;
-      }
-    }
-    });
-    this.productsData$.next(filterProductsData);
   }
- 
+    
+  private filterForType(product:IProduct, idSelectedOption:number){
+     switch (idSelectedOption) {
+          //filter products by bestseller
+          case 1:
+            if (product.bestSeller) return true;
+            break;
+          //filter porducts by available
+          case 2:
+            if (product.quantity>0)return true;
+            break;
+          //filter products by Exhausted 
+          case 3:
+            if (product.quantity<1)return true;
+          default:
+            return false;
+            break;
+      }
+  }
+  
+  private filterForQuantity(product:IProduct, idSelectedOption:number){
+    switch (idSelectedOption) {
+      case 1:
+        if (product.price >30000)return true;
+        break;
+      case 2:
+        if (product.price <10000)return true;
+        break;
+      default:
+        return false;
+    }
+  }
+
   clearSelectedCategory(){
     this.selectedCategory = null;
     this.setProductData();
   }
   clearSelectedType(){
-    this.selectType = null;
+    this.selectedType = null;
+    this.setProductData();
+  }
+  clearSelectedQuantity(){
+    this.selectedQuantity = null;
     this.setProductData();
   }
 }
